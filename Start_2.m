06fc20696e1cedde39973012515644d67e010cf8
Robby <ta7382@gmail.com>
2019-05-18 11:48:27 +0800

    Normalize feq data

--- Start_2.m
+++ Start_2.m
@@ -25,6 +25,25 @@ for s=1:length(getNames)
     for f=1:length(files)  
         HRVAll = [ HRVAll ; readtable(['./export/' NAME '/' cell2mat(files(f)) ])];
     end
+    
+    HRVAll = cell2mat(table2cell(HRVAll));
+    
+    % min-max scaling
+    % TP
+    HRVAll(:,10) = rescale(HRVAll(:,10),1,100);
+    % HF
+    HRVAll(:,11) = rescale(HRVAll(:,11),1,100);
+    % LF
+    HRVAll(:,12) = rescale(HRVAll(:,12),1,100);
+    % VLF
+    HRVAll(:,13) = rescale(HRVAll(:,13),1,100);
+    % ULF
+    HRVAll(:,14) = rescale(HRVAll(:,14),1,100);
+    
+    Header = {'Stage','NNAvg','NN','MeanRR','SDNN','NN50','pNN50','RMSSD','SDRM','TP','HF','LF','VLF','ULF','nLF','nHF','LFHF_Ratio'};
+
+    HRVAll = array2table(HRVAll, 'VariableNames', Header);
+
     writetable(HRVAll, [PATH NAME '/' 'HRV.csv']);
     % ======================================================
     end
